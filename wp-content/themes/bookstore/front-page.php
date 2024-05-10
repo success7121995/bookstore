96003f93b89a069bad2c566e84c09b4192bea74f
Success7121995 <success7121995@gmail.com>
2024-05-10 17:05:50 +0800

    update front page

--- wp-content/themes/bookstore/front-page.php
+++ wp-content/themes/bookstore/front-page.php
@@ -7,14 +7,21 @@
 get_header();
 ?>
 <div class="container main-content">
-    <?php 
+<?php 
     if (have_posts()):
         while(have_posts()):
             the_post();
             the_content();
         endwhile;
     endif;
-    ?>
+?> 
+    <!-- New Book Releases -->
+    <h3 class="wp-block-heading">New Book Release</h3>
+    <?php echo do_shortcode('[display_new_releases]', true); ?>
+    <!-- Recommendations -->
+    <h3 class="wp-block-heading">Recommendations</h3>
+    <?php echo do_shortcode('[display_recommendations]', true); ?>
+
 </div>
 
 <?php
